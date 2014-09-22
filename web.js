6dc0f2bf31ae4fe10a349561c07ee12f98873ec3
Tey Bannerman <tey.bannerman@gmail.com>
2014-09-22 05:14:58 +0200

    web.js tweaks for heroku

--- web.js
+++ web.js
@@ -1,14 +1,7 @@
-/**  
-  * @desc basic static express server to host our app
-  * @author Tey Bannerman teybannerman@gmail.com 
-*/  
-
-var express = require('express'); 
+var gzippo = require('gzippo');
+var express = require('express');
 var app = express();
-
-// compress content
-//app.use(express.compress());
-
-app.use(express.static(__dirname + '/'));
-
-app.listen(process.env.PORT || 3000);
+ 
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
