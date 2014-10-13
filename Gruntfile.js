704f39500e9d4595ae6085b713fb026ceff18ad9
Tey Bannerman <tey.bannerman@gmail.com>
2014-10-13 23:39:31 +0200

    Fix gruntfile copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
     copy: {
       main: {
         files: [
-          {src: ['img/**'], dest: 'dist/'},
+          {src: ['images/**'], dest: 'dist/'},
           {src: ['bower_components/font-awesome/fonts/**'], dest: 'dist/',filter:'isFile',expand:true}
           //{src: ['bower_components/angular-ui-utils/ui-utils-ieshiv.min.js'], dest: 'dist/'},
           //{src: ['bower_components/select2/*.png','bower_components/select2/*.gif'], dest:'dist/css/',flatten:true,expand:true},
