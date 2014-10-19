a3f3c099596b1480cfc03556e21c39c34ec7f5a0
Tey Bannerman <tey.bannerman@gmail.com>
2014-10-20 01:17:49 +0200

    Company list template scripts

---
+++ directive/company-list/company-list.js
@@ -0,0 +1,14 @@
+angular.module('skillscap-proto').directive('companyList', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+
+		},
+		templateUrl: 'directive/company-list/company-list.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
