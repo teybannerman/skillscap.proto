08852480a432e7149b5e9a8d7286e3d8f8a323b1
Tey Bannerman <tey.bannerman@gmail.com>
2014-11-01 00:50:19 +0100

    Updated web.js to try and fix font-awesome

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express();
 //app.use(express.logger('dev'));
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.use('', express.static(__dirname + '/dist'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.listen(process.env.PORT || 5000);
 
 
