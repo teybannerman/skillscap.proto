24be6a980c12cdd74021d7860cf97fe2caea974a
Tey Bannerman <tey.bannerman@gmail.com>
2014-11-01 02:29:07 +0100

    Fixed mobile menu slideUp

--- app.js
+++ app.js
@@ -120,5 +120,11 @@ angular.module('skillscap-proto').run(function($rootScope, $state, $stateParams,
     $rootScope.$broadcast('savestate');
   };
 
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+          $(this).collapse('hide');
+      }
+  });
+
 });
 
