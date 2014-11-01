8087ae12315725e36f49f5ddcb03e17c7a109818
Tey Bannerman <tey.bannerman@gmail.com>
2014-11-01 02:11:12 +0100

    Updated gitignore to fix fonts
    gitignore was excluding the dist bower_components folder from being
    copied to heroku. Now fixed.

--- web.js
+++ web.js
@@ -5,7 +5,6 @@ var app = express();
 //app.use(express.logger('dev'));
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.use('', express.static(__dirname + '/dist'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.listen(process.env.PORT || 5000);
 
 
